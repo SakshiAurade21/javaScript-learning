4ec0f08b6d9c196f3210598bc3ff275e530edcd9
SakshiAurade21 <sakshiaurade21@gmail.com>
2025-07-22 20:05:55 +0530

    creating repo

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,9 @@
+
+const accountNumber="234562782";
+let accoutName="Sakshi";
+var accountPassword ="sakshi123";
+accountCity ="Pune";
+
+
+console.log(accountNumber);
+console.table([accountNumber,accountPassword,accoutName]);
