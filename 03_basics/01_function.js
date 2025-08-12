3954e309352ba28a76e601a9abd178c3c1ed5a92
SakshiAurade21 <sakshiaurade21@gmail.com>
2025-08-12 21:21:59 +0530

    function in js

---
+++ 03_basics/01_function.js
@@ -0,0 +1,29 @@
+
+function sayname(){
+    console.log("S");
+    console.log("A");
+    console.log("k");
+    console.log("S");
+    console.log("H");
+    console.log("I");
+}
+
+sayname()
+
+// result=sayname()// if we doesnt passed any value its return undefined
+// console.log(result);
+
+
+// function addNumber (number1 ,number2){
+//     return number1+number2;
+// }
+// resultOfAddition=addNumber(11,11);
+// console.log(resultOfAddition);
+
+
+function logIn(username="sakshi"){
+    return`hey my name is ${username}, nice to meet you !`;}
+
+
+console.log(logIn());//sakshi
+// console.log(logIn("sunny"));//sunny
