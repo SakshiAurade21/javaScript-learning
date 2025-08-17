6758eb8a9af398266661b6ff59d5f5162f39de89
SakshiAurade21 <sakshiaurade21@gmail.com>
2025-08-17 22:24:01 +0530

    learned if switch case in js

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,19 @@
+const day=4;
+switch (day) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("wenesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+
+    default:
+        console.log("heyyyyy");
+        break;
+}
